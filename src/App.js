9365f37284417410b29c097b09df94f7ea4f74f0
Jaspal Singh <jaspals.ug21.cs@nitp.ac.in>
2024-01-07 17:37:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import Missing from "./Components/Missing";
 import Nav from "./Components/Nav";
 import NewPost from "./Components/NewPost";
 import PostPage from "./Components/PostPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Routes>
-        <Route path="/Header" component={ <Header/> }></Route>
-      </Routes>
-    </Router>
-      {/* <Header />
+      <Header />
       <Nav />
       <Home />
       <NewPost />
       <PostPage />
       <About />
       <Missing />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 };
